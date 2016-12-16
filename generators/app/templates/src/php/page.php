36c371e433154b1136d66224f4ab0e529fdb6e55
Jari Jokinen <info@jarijokinen.com>
2016-12-16 20:56:08 +0200

    Add container for page content

--- generators/app/templates/src/php/page.php
+++ generators/app/templates/src/php/page.php
@@ -1,8 +1,14 @@
 <?php get_header(); ?>
 
-<?php while (have_posts()): the_post(); ?>  
-  <?php the_title('<h1>', '</h1>'); ?>
-  <?php the_content(); ?>
-<?php endwhile; ?>
+<main>
+  <?php while (have_posts()): the_post(); ?>
+    <article>
+      <div>
+        <?php the_title('<h1>', '</h1>'); ?>
+        <?php the_content(); ?>
+      </div>
+    </article>
+  <?php endwhile; ?>
+</main>
 
 <?php get_footer(); ?>
