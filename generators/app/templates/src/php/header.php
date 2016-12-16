c653486cf6d4c93593e178ca2c0b5b196cf8c127
Jari Jokinen <info@jarijokinen.com>
2016-12-16 20:55:24 +0200

    Add navbar

--- generators/app/templates/src/php/header.php
+++ generators/app/templates/src/php/header.php
@@ -7,3 +7,23 @@
     <?php wp_head(); ?>
   </head>
   <body>
+    <header>
+      <div>
+        <?php if (is_front_page()): ?>
+          <h1><?php bloginfo('name'); ?></h1>
+        <?php else: ?>
+          <a href="/"><?php bloginfo('name'); ?></a>
+        <?php endif; ?>
+        <nav>
+          <ul>
+            <?php
+              wp_nav_menu(array(
+                'container' => '',
+                'items_wrap' => '%3$s',
+                'theme_location' => 'header-menu'
+              ));
+            ?>
+          </ul>
+        </nav>
+      </div>
+    </header>
